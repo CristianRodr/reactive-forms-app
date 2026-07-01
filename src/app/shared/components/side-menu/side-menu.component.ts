4f191ac120fd69a85a42bd6bce74ab58432b3e01
an1me <an1me@DESKTOP-QBK9NTJ>
2026-06-30 21:28:19 -0500

    ajuste html proyecto practica

--- src/app/shared/components/side-menu/side-menu.component.ts
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { reactiveRoutes } from '../../../reactive/reactive.routes';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+const reactiveItems = reactiveRoutes[0].children ?? [];
 
 @Component({
   selector: 'app-side-menu',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  reactiveMenuItems: MenuItem[] = reactiveItems
+    .filter((item) => item.path !== '**')
+    .map((route) => ({
+      title: `${route.title}`,
+      route: `reactive/${route.path}`,
+    }));
+
+  authMenuItems: MenuItem[] = [
+    {
+      title: 'Registro',
+      route: './auth',
+    },
+  ];
+
+  countryMenuItems: MenuItem[] = [
+    {
+      title: 'Paises',
+      route: './country',
+    },
+  ];
+}
